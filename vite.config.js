10b652dc9478755bfac2cf83e66d34ba3362c4f9
harsh <gkutsavpalaceoffical@gmail.com>
2024-02-11 11:05:41 +0530

    gggsds

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'           
+
+dotenv.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{port:"10000"}
+  server:{port:process.env.PORT}
 })
