87c3ccf2d5a9f7e639cd425e031b384763a2c423
harsh <gkutsavpalaceoffical@gmail.com>
2024-02-13 14:49:32 +0530

    ccv

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv'           
-
-dotenv.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{port:process.env.PORT}
+  server: {
+    port: process.env.PORT || 4000// Using parseInt to ensure the port is a number and providing a default value of 3000 if PORT is not set
+  }
 })
